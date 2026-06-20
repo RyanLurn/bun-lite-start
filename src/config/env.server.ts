51adfe9f29739e007dbcb35561c3cc6395c40c4b
Ryan Luong <lumintriet@gmail.com>
2026-06-20 08:55:49 +0700

    feat(config/env): add LOG_LEVEL env.

--- src/config/env.server.ts
+++ src/config/env.server.ts
@@ -1,7 +1,19 @@
+import type { Level } from "pino";
+
 import { z } from "zod";
+
+export const logLevels: Level[] = [
+  "fatal",
+  "error",
+  "warn",
+  "info",
+  "debug",
+  "trace",
+] as const;
 
 const envValidator = z.object({
   NODE_ENV: z.enum(["development", "testing", "staging", "production"]),
+  LOG_LEVEL: z.enum(logLevels).default("info"),
 });
 
 export const env = envValidator.parse(process.env);
